04ac3fe291cc0f6ae64cde79204a6a74580f2924
pythonscriptkiddie <38157432+pythonscriptkiddie@users.noreply.github.com>
2019-07-15 08:52:21 -0400

    Add files via upload

---
+++ news/news_article.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 05:44:38 2019
+
+@author: thomassullivan
+"""
+
+from newspaper import Article as NewsItem
+#from objects import Article
+
+def get_article_from_url(url):
+    new_article = NewsItem(url=url)
+    new_article.download()
+    new_article.build()
+    new_article.nlp()
+    #print(new_article.__dict__)
+    #print(new_article)
+    return new_article
+
+def get_summary(new_article):
+    return new_article.summary
+
+def get_article_summary(url):
+    try:
+        new_article = get_article_from_url(url)
+        return get_summary(new_article)
+    except Exception as e:
+        description = 'Not found due to {0}'.format(e)
+        return description
+        
+
+if __name__ == '__main__':
+    new_link = input('Please enter the link: ' )
+    new_article = get_article_from_url(new_link)
+    print(new_article.url)
